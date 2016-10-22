06c8edfdd3bdf16e84db468a2cd9f1b1680e4dfb
Benson <bensonjaminhan@gmail.com>
2016-10-22 16:13:35 -0700

    tabs and css

--- main.js
+++ main.js
@@ -7,7 +7,56 @@ $(function (){
       console.log(data);
       var template = $("#locations-template").html();
       var templateScript = Handlebars.compile(template);
-      $('#eqdata').html(templateScript(data));
+      $('#sigdata').html(templateScript(data));
     }
   });
+
+  $.ajax({
+    type: "GET",
+    url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson",
+    //url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+    success: function(data){
+      console.log(data);
+      var template = $("#locations-template").html();
+      var templateScript = Handlebars.compile(template);
+      $('#m45data').html(templateScript(data));
+    }
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson",
+    //url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+    success: function(data){
+      console.log(data);
+      var template = $("#locations-template").html();
+      var templateScript = Handlebars.compile(template);
+      $('#m25data').html(templateScript(data));
+    }
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson",
+    //url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+    success: function(data){
+      console.log(data);
+      var template = $("#locations-template").html();
+      var templateScript = Handlebars.compile(template);
+      $('#m10data').html(templateScript(data));
+    }
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson",
+    //url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+    success: function(data){
+      console.log(data);
+      var template = $("#locations-template").html();
+      var templateScript = Handlebars.compile(template);
+      $('#alldata').html(templateScript(data));
+    }
+  });
+
 });
